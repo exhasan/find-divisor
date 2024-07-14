c5e9d9c939961b86e08b225ba7cfa77f942242b2
exhasan <mdemonhasan6768@gmail.com>
2024-07-14 11:59:56 +0600

    sumofdivisor

--- divisor.cpp
+++ divisor.cpp
@@ -4,14 +4,25 @@ int main()
 {
     int n;
     cin>>n;
+    int sum=0;
     for(int i=1;i<=sqrt(n);i++)
     {
         if(n%i==0)
         {
             if(i==n/i)
+            {
                 cout<<i<<" ";
+                sum+=i;
+            }
             else
+            {
                 cout<<i<<" "<<n/i<<" ";
+                sum+=i+n/i;
+            }
+                
         }
     }
+    cout<<"\nSum of divisor: "<<sum;
+    return 0;
+
 }
